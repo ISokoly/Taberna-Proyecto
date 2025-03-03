523a2708666dc8bfdfaa43afc6f8a5e26ab2bc3b
Sokoly <therockplans@gmail.com>
2025-03-03 17:57:58 -0500

    er

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { HeaderComponent } from "../header/header.component";
+import { ViewComponent } from "../view/view.component";
 
 @Component({
-  imports: [SidebarComponent, HeaderComponent],
+  imports: [SidebarComponent, HeaderComponent, ViewComponent],
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
