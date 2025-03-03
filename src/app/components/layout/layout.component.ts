ce46445cca7ce2b28ce9298d73beb53f1b622583
Sokoly <therockplans@gmail.com>
2025-03-03 17:35:45 -0500

    lk

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, HeaderComponent],
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
